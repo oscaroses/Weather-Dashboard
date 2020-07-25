ab1b291f96be8f4ad582cbc42a3acca71ad32530
PeeWee <oscar.oses.1127@gmail.com>
2020-07-24 20:45:57 -0400

    added function to diplay current city info. not wokring :(

--- README.md
+++ README.md
@@ -44,3 +44,10 @@ You are required to submit the following for review:
 
 - - -
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+
+create intial files
+create basic html layout with rows and columns.
+create java script
+get api key
+creat global variables
+create event listeners for search.
--- javascript.js
+++ javascript.js
@@ -1,6 +1,34 @@
-var city = $("#Search-Btn").val()
+let city = " ";
+var apiKey = "d2dc4b5a67f8f43f9ff13956727536e2";
 
+$("#Search-Btn").on("click", function () {
+  if (city == " ") {
+    return;
+  } else {
+    var city = $("#input-city").val().trim();
+  }
+  getForecast(city);
+});
 
-$("#Search-Btn").on("click", function {
+function getForecast(city) {
+  let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
 
-})
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+}
+
+function getCurrentCity(response) {
+  $("#main-card").empty()
+
+  var card = $("<div>").addClass("card");
+  var cardBody = $("<div>").addClass("card-body")
+  var cityName = $("<h1>").addClass("card-title").text(response.name);
+
+  $("#main-card").append(card)
+  card.append(cardBody)
+  cardBody.append(cityName)
+}
