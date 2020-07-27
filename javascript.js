b02215bed73650bf511015658af56a652b997bdb
PeeWee <oscar.oses.1127@gmail.com>
2020-07-27 16:25:53 -0400

    added function for 5 day forecast. nothing works :D

--- javascript.js
+++ javascript.js
@@ -24,9 +24,8 @@ function getForecast(city) {
 
 
 
-    let date = response.dt
-    let cityDate = new Date(date * 1000).toLocaleDateString("en-US")
-
+    var date = response.dt
+    var cityDate = new Date(date * 1000).toLocaleDateString("en-US")
     var wIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
     var card = $("<div>").addClass("card");
     var cardBody = $("<div>").addClass("card-body")
@@ -38,6 +37,55 @@ function getForecast(city) {
     card.append(cardBody)
     cardBody.append(cityName, temperature, humidity, windSpeed)
     $(".card-title").append(wIcon)
+
+
+    let lat = response.data.coord.lat;
+    let lon = response.data.coord.lon;
+    let uvUrl = "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+      key +
+      "&lat=" +
+      lat +
+      "&lon=" +
+      lon;
   });
+
+  $.ajax({
+
+    url: uvUrl,
+    method: "GET",
+  }).then(function (uvresponse) {
+    console.log(uvresponse);
+  })
+
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (fiveresponse) {
+    console.log(response);
+
+    for (i = 0; i < 6; i++) {
+      var fiveDay = new Date(fiveresponse.daily[i].dt * 1000).toLocaleDateString("en-US");
+      var dailyPic = fiveresponse.daily[i].weather[0].icon;
+      var dailyIcon = "https://openweathermap.org/img/wn/" + dailyPic + "@2x.png";
+
+      var dateCard = $("<div>").addClass("card daily")
+      var iconImg = $("<img>").attr("src", dailyIcon)
+      iconImg.attr("alt", "Weather Icon")
+      var fiveHigh = fiveresponse.daily[i].temp.max;
+      var fiveLow = fiveresponse.daily[i].temp.min;
+      var fiveHum = fiveresponse.daily[i].humidity;
+
+      var info1 = $("<p>").text(fiveHigh)
+      var info2 = $("<p>").text(fiveLow)
+      var info3 = $("<p>").text(fiveHum)
+
+      $("#forecast").append(dateCard + i + fiveDay)
+      dateCard.append(info1, info2, info3)
+    }
+  })
+
+
+
 
 }
