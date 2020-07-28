d6da613f71eb394094f093cffdb43473a5238305
PeeWee <oscar.oses.1127@gmail.com>
2020-07-27 22:00:55 -0400

    added styles. new issue where blank button is created when you search for nothing

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
+//Global variables
 let cityHist = [];
 let city = " ";
 var apiKey = "d2dc4b5a67f8f43f9ff13956727536e2";
 var fiveMain = document.getElementById("FC-P")
 
+//This is the main function that gets the city information through out the code.
 function getForecast(city) {
   let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
 
+  //Here we get the info for the searched city, then append it to the html us ing JQuery.
   $.ajax({
     url: queryUrl,
     method: "GET",
@@ -28,6 +31,7 @@ function getForecast(city) {
     cardBody.append(cityName, temperature, humidity, windSpeed)
     $(".card-title").append(wIcon)
 
+    //We also set the lattitude and longitude to get the UV index and 5-Day forecast.
     let lat = response.coord.lat;
     let lon = response.coord.lon;
     let uvUrl = "https://api.openweathermap.org/data/2.5/uvi?appid=" +
@@ -37,6 +41,7 @@ function getForecast(city) {
       "&lon=" +
       lon;
 
+    //Here we get UV info and append it to the main city info card. Depending on the value of the UV index, we change the color of the button.
     $.ajax({
       url: uvUrl,
       method: "GET",
@@ -75,7 +80,7 @@ function getForecast(city) {
       "&exclude=current,minutely,hourly&units=imperial&appid=" +
       apiKey;
 
-
+    //Here we get the 5-day forecast and run it through a for loop. Then we get the we create a card for the following 5 days and append it using JQuery.
     $.ajax({
       url: dailyUrl,
       method: "GET",
@@ -108,6 +113,7 @@ function getForecast(city) {
   });
 }
 
+// Here the search history is stored with JSON. Then we create buttons for each item searched for. It is set to only display the 5 most recent searches.
 function oldCity(city) {
   if (cityHist.length == 6) {
     cityHist.splice(0, 1)
@@ -130,6 +136,7 @@ function oldCity(city) {
   localStorage.setItem("cityHist", JSON.stringify(cityHist))
 }
 
+//Here the search history is restored if the page is refreshed.
 function getHist() {
 
   if (localStorage.getItem("cityHist") !== null) {
@@ -151,9 +158,9 @@ function getHist() {
     getForecast(cityLoad)
   }
 }
-
 getHist();
 
+//This is an event listener for the search button.
 $("#Search-Btn").on("click", function () {
   fiveMain.style.display = "inline"
   if (city == " ") {
@@ -165,7 +172,9 @@ $("#Search-Btn").on("click", function () {
   oldCity(city)
 });
 
+//This is and event listener for the search history.
 $(".li-btn").on("click", function () {
+  fiveMain.style.display = "inline"
   var histBtn = $(this).text()
   getForecast(histBtn)
 })
